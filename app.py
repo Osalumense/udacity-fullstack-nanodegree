d6e84ad68890eb99e5498f05e338b4260befc1f0
Stephen <harkugbeosaz@gmail.com>
2022-05-26 16:10:51 +0100

    Initialized the to do flask application

--- app.py
+++ app.py
@@ -1,30 +1,57 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/example'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
+    # __tablename__ = 'persons'
+    # id = db.Column(db.Integer, primary_key=True)
+    # name = db.Column(db.String(), nullable=False)
+
+    # def __repr__(self):
+    #     return f'<Person ID: {self.id}, name: {self.name}>'
 class Person(db.Model):
-    __tablename__ = 'persons'
+    __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
-        return f'<Person ID: {self.id}, name: {self.name}>'
+        return f'<Todo {self.id}  {self.description}>'
 
-db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():   
+   body={}
+   error = False
+   try: 
+       description =  request.get_json()['description']
+       todo = Todo(description=description)
+       body['description'] = todo.description
+       db.session.add(todo)
+       db.session.commit()
+   except:        
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+   finally:
+        db.session.close()           
+        if  error == True:
+            abort(400)
+        else:            
+            return jsonify(body)
 
 @app.route('/')
 def index():
-    person = Person.query.first()
-    return "Hello " + person.name
-
-
+    return render_template('index.html', data=Todo.query.all())
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
